056e9c1433a66884fd490df4e15e43e67c61ac83
Eduardo <orthmann2811@gmail.com>
2023-10-15 22:22:21 -0300

    fix swagger

--- src/main/java/com/example/simplelibrary/SimpleLibraryApplication.java
+++ src/main/java/com/example/simplelibrary/SimpleLibraryApplication.java
@@ -1,12 +1,23 @@
 package com.example.simplelibrary;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Server(url = "https://simple-library-production.up.railway.app", description = "Production server")
-@Server(url = "http://localhost:8080", description = "Local development server")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Simple Library API",
+                version = "1.0.0",
+                description = "This is a simple library API made for the module of Spring boot in the +devs2blu program.\""
+        ),
+        servers = {
+                @Server(url = "https://simple-library-production.up.railway.app", description = "Production server"),
+                @Server(url = "http://localhost:8080", description = "Local development server")
+        }
+)
 public class SimpleLibraryApplication {
 
     public static void main(String[] args) {
